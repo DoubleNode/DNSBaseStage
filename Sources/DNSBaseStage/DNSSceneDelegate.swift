adf62445f9597c0add68f2c6515997e232fdc6e5
Darren Ehlers <me@darrenehlers.com>
2019-11-23 14:50:17 -0600

    Made DNSSceneDelegate open

--- Sources/DNSBaseStage/DNSSceneDelegate.swift
+++ Sources/DNSBaseStage/DNSSceneDelegate.swift
@@ -9,14 +9,14 @@
 import DNSCore
 import UIKit
 
-public class DNSSceneDelegate: UIResponder, UIWindowSceneDelegate {
+open class DNSSceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     public var coordinator: DNSCoordinator?
     public var window: UIWindow?
 
     // MARK: - UIWindowSceneDelegate methods
 
-    public func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    open func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new
@@ -26,7 +26,7 @@ public class DNSSceneDelegate: UIResponder, UIWindowSceneDelegate {
         coordinator?.start()
     }
 
-    public func sceneDidDisconnect(_ scene: UIScene) {
+    open func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
@@ -37,25 +37,25 @@ public class DNSSceneDelegate: UIResponder, UIWindowSceneDelegate {
         coordinator = nil
     }
 
-    public func sceneDidBecomeActive(_ scene: UIScene) {
+    open func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
         (coordinator as? DNSSceneCoordinatorProtocol)?.didBecomeActive()
     }
 
-    public func sceneWillResignActive(_ scene: UIScene) {
+    open func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
         (coordinator as? DNSSceneCoordinatorProtocol)?.willResignActive()
     }
 
-    public func sceneWillEnterForeground(_ scene: UIScene) {
+    open func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
         (coordinator as? DNSSceneCoordinatorProtocol)?.willEnterForeground()
     }
 
-    public func sceneDidEnterBackground(_ scene: UIScene) {
+    open func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
